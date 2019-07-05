231d1726cc2905b6e80bf1094b3713172dc4c77c
Mark Attree <markattree@hotmail.com>
2019-07-05 10:45:26 +0100

    Code tidy up

--- roger-ebot/index.js
+++ roger-ebot/index.js
@@ -14,40 +14,70 @@ client.on('message', msg => {
 
   if (!messageContent.startsWith(prefix) || msg.author.bot) return;
 
-  var args = messageContent.slice(prefix.length + 1).split(/ +/).shift();
+  var args = getArgs(messageContent);
 
   if (args === 'help') {
-    var category = client.channels.find(channel => channel.name === 'Spoiler megathreads' && channel.type === 'category');
-
-    if (category) {
-      var reply = 'Available channels: \n';
-      category.children.forEach(child => {
-        if (child.name !== 'list') {
-          reply += child + '\n';
-        }
-      });
-
-      msg.reply(reply);
-    }
+    var megathreads = getAllSpoilerMegathreads(client);
+    var reply = megathreads !== '' ? 'Available megathreads:' + megathreads : 'There are no available megathreads';
+    msg.reply(reply);
     return;
   }
 
-  if (args.startsWith('#')) {
-    args = args.substring(1);
-  }
+  var channel = args.startsWith('<#') && args.endsWith('>') ? getChannelById(client, args) : getChannelByName(client, args);
 
-  var channel = client.channels.find(channel => channel.name === args);
-
-  console.log(channel);
-
-  if (channel != null) {
-    msg.reply('Adding you to ' + channel);
-    channel.overwritePermissions(msg.member.user.id, { VIEW_CHANNEL: true });
+  if (channel) {
+    addUserToChannel(channel, msg.member.user.id)
+      .then(msg.reply('You have been added to ' + channel))
+      .catch(err => {
+        console.error(err);
+        msg.reply('Sorry, there was an error adding you to the channel');
+      });
   } else {
-    msg.channel.send('That channel does not exist');
+    msg.reply('That channel does not exist');
   }
 });
 
 client.on('error', console.error);
+
+function getArgs (messageContent) {
+  return messageContent.slice(prefix.length).trim();
+}
+
+function getSpoilerMegathreadsCategory (client) {
+  return client.channels.find(channel => channel.name === 'Spoiler megathreads' && channel.type === 'category');
+}
+
+function getChannelByName (client, args) {
+  return client.channels.find(channel => channel.name === args);
+}
+
+function getChannelById (client, args) {
+  var extractedId = extractId(args);
+  return client.channels.get(extractedId);
+}
+
+function extractId (args) {
+  return args.substring(2, args.length - 1);
+}
+
+function addUserToChannel (channel, userId) {
+  return channel.overwritePermissions(userId, { VIEW_CHANNEL: true });
+}
+
+function getAllSpoilerMegathreads (client) {
+  var spoilerMegathreadsCategory = getSpoilerMegathreadsCategory(client);
+
+  if (spoilerMegathreadsCategory) {
+    var threadList = '';
+
+    spoilerMegathreadsCategory.children.forEach(child => {
+      if (child.name !== 'list') {
+        threadList += '\n' + child;
+      }
+    });
+  }
+
+  return threadList;
+}
 
 client.login(auth.token);
